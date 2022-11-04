123c1e76cf0ffca50703384573b2af3fa4c7ad75
Iptysam <mukadamiptisam@gmail.com>
2022-11-04 16:57:01 +0300

    Update files

--- react-todo-app/src/classBased/components/TodosList.js
+++ react-todo-app/src/classBased/components/TodosList.js
@@ -6,7 +6,8 @@ class TodosList extends React.Component {
         return (
             <ul>
         {this.props.todos.map(todo => (
-          <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps} />
+          <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps} deleteTodoProps={this.deleteTodoProps} setUpdate={this.props.setUpdate}
+          />
         ))} 
       </ul>
         )
---
+++ react-todo-app/src/functionBased/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+const About = () => {
+  return(
+    <div className="about__content">
+        <p className="about">
+      React Todo App
+      This is a simple app that adds a list of elements.
+      You can add, edit and delete any item added
+    </p>
+  </div>
+  )
+  
+}
+export default About
